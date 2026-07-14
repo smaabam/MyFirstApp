da9ce667157c9640b940b291ca305c734b55d440
smaabam <shaaban.2023g10@gmail.com>
2026-07-14 09:28:40 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.3.0")
     implementation("com.google.api-client:google-api-client-android:2.7.0")
     implementation("com.google.http-client:google-http-client-gson:1.45.0")
-    implementation("com.google.apis:google-api-services-drive:v3-rev20240806-2.0.0")
+    implementation("com.google.apis:google-api-services-drive:v3-rev20251210-2.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.apache.poi:poi-ooxml:5.3.0")
 }
