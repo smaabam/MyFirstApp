db0dd92043772df8ad3dd8b676be106394b7064e
smaabam <shaaban.2023g10@gmail.com>
2026-07-14 09:37:34 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,16 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
+
+       compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+}
 
     buildFeatures { compose = true; buildConfig = true }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
